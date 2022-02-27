581f61dc046cb07bb8e5a843478fb226191fe2a3
CodeGuyUg <86480489+jumatech78@users.noreply.github.com>
2022-02-27 15:57:11 +0300

    updated the navbar

--- instagram_clone/src/components/Navbar/index.js
+++ instagram_clone/src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './navbarstyles.css'
+import Badge from '@mui/material/Badge';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import SendIcon from '@mui/icons-material/Send';
 import AddIcon from '@mui/icons-material/Add';
 import ExploreIcon from '@mui/icons-material/Explore';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 function Navbar(){
     return(
@@ -19,7 +20,9 @@ function Navbar(){
             </div>
             <div className='navbar_icons'>
                 <HomeIcon className='navbar_icon'/>
-                <SendIcon className='navbar_icon'/>
+                <Badge badgeContent={2} color="primary">
+                <SendIcon className='navbar_icon' style={{ transform: 'rotate(320deg)' }}/>
+                </Badge>
                 <AddIcon className='navbar_icon'/>
                 <ExploreIcon className='navbar_icon'/>
                 <FavoriteBorderIcon className='navbar_icon'/>
