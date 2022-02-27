049662f8bd1d38e8d0873ef0f4431492d14eede8
CodeGuyUg <86480489+jumatech78@users.noreply.github.com>
2022-02-27 15:15:57 +0300

    created navbar

---
+++ instagram_clone/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      {/* Welcome! */}
+    </div> 
+  )
+}
+
+export default App;
---
+++ instagram_clone/src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './navbarstyles.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SendIcon from '@mui/icons-material/Send';
+import AddIcon from '@mui/icons-material/Add';
+import ExploreIcon from '@mui/icons-material/Explore';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+function Navbar(){
+    return(
+        <div className='navbar'>
+            <div className='navbar_brand'>
+                <img className='navbar_logo' src='/logo.png'/>
+            </div>
+            <div className='search_section'>
+                <SearchIcon size={10} className='search_icon'/>
+                <input className='search-input' placeholder='search'/>
+            </div>
+            <div className='navbar_icons'>
+                <h3>icons</h3>
+                {/* <HomeIcon/>
+                <SendIcon/>
+                <AddIcon/>
+                <ExploreIcon/>
+                <FavoriteBorderIcon/> */}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
