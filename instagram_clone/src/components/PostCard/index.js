d3763c6230339e12ad88fea3fa47ca55dbb460b9
CodeGuyUg <86480489+jumatech78@users.noreply.github.com>
2022-03-22 22:37:45 +0300

    footer updates

--- instagram_clone/src/components/PostCard/index.js
+++ instagram_clone/src/components/PostCard/index.js
@@ -21,18 +21,37 @@ function PostCard() {
         </div>
       </div>
       <div className="post_card_body">
-        <img src='https://images.unsplash.com/photo-1647831570034-51d43eac0694?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'/>
+        <img src="https://images.unsplash.com/photo-1647831570034-51d43eac0694?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
       </div>
       <div className="card_footer">
         <div className="card_footer_chat">
           <div>
-            <FavoriteBorder/>
-            <SendIcon/>
+            <FavoriteBorder />
+            <SendIcon style={{marginLeft:10}} />
           </div>
           <div>
-            <BookmarkBorder/>
+            <BookmarkBorder />
           </div>
         </div>
+      </div>
+      <div style={{
+        display:'flex',
+        alignItems:'center',
+      }}>
+        <Avatar style={{
+          marginRight:20,
+          height:25,
+          width:25,
+          marginLeft:20
+        }} src='https://images.unsplash.com/photo-1647613560920-9576ec9a4537?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'/>
+        <h6>Liked by <span style={{fontWeight:'900'}}>JumaTech</span></h6>
+      </div>
+      <div>
+        <h5 style={{
+          fontWeight:'100',
+          marginTop:2,
+          padding:20
+        }}>5 hours ago</h5>
       </div>
     </div>
   );
